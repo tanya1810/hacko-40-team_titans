2a4a5d42de774c2d5636165a5c7badc53d5e94cc
tanya1810 <tanya.sood.311@gmail.com>
2021-03-27 03:38:50 +0530

    feeds section frontend complete

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/index.html')
+    return render(request, 'home/feed_detail.html')
